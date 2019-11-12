ad1c50b6667879f385eb411c08f3345e9afbb172
Matvey Loshakov <matveyloshakov@gmail.com>
2019-11-12 23:22:45 +0100

    Better dataframe creation with different types

--- download_data.py
+++ download_data.py
@@ -1,6 +1,6 @@
 import urllib.request
-from bs4 import BeautifulSoup
 import pandas as pd
+from bs4 import BeautifulSoup
 from sql_handle import create_database
 
 
@@ -64,18 +64,49 @@ def create_season_dataframe(table):
     """
     df = pd.DataFrame(table[1:], columns=table[0])  # creating dataframe
 
-    # going through all the columns and specifying data types because pandas cannot do it more beautifully
-    df["Laps"] = df["Laps"].astype(int)
+    # going through all the columns and specifying data types
+    df["Laps"] = pd.to_numeric(df["Laps"], errors='coerce').astype('Int64')
     df["Grand Prix"] = df["Grand Prix"].astype(str)
-    df["Winner"] = df["Winner"].astype(str)
+    df["Winner"] = df["Winner"].astype(str).apply(lambda x: x.replace(" ", ""))
     df["Car"] = df["Car"].astype(str)
     df["Date"] = pd.to_datetime(df["Date"]).dt.date
     df["Time"] = pd.to_datetime(df["Time"], format="%H:%M:%S.%f").dt.time
     return df
-    
 
-sql_engine, sql_session, sql_base = create_database()
-sql_options = dict(if_exists='replace', index=False, method='multi')
+
+def create_race_dataframe(table):
+    """
+    Create race dataframe from results table
+    :param table: results table from create_results_table() func
+        Actually a list of lists!
+    :return: race dataframe
+    """
+    df = pd.DataFrame(table[1:], columns=table[0])  # creating dataframe
+
+    # going through all the columns and specifying data types
+    df["Pos"] = pd.to_numeric(df["Pos"], errors='coerce').astype('Int64')
+    df["No"] = df["No"].astype(int)
+    df["Laps"] = pd.to_numeric(df["Laps"], errors='coerce').astype('Int64')
+    df["Car"] = df["Car"].astype(str)
+    df["Driver"] = df["Driver"].astype(str).apply(lambda x: x.replace(" ", ""))
+    df["PTS"] = pd.to_numeric(df["PTS"], errors='coerce').astype('Int64')
+    return df
+
+
+def load_data_to_database(dataframe, table_name: str, engine):
+    """
+    Creates a table from provided dataframe in specified database with specified name
+    Overwrites existing table!
+    :param dataframe: dataframe to create table from
+    :param table_name: name of the table
+    :param engine: where to create table
+    """
+    sql_options = dict(if_exists='replace', index=False, method='multi')
+    dataframe.to_sql(table_name, engine, **sql_options)  # truncates the table
+
+
+db_engine, sql_session, sql_base = create_database()
+
 race_number = 94  # arbitrary number from which FIA starts their race count
 for season_year in range(1950, 1951):
     soup = BeautifulSoup(urllib.request.urlopen(create_year_url(season_year)), features="lxml")
@@ -83,21 +114,22 @@ for season_year in range(1950, 1951):
     season_df = create_season_dataframe(season_table)
 
     print(season_df)
-    print(season_df.dtypes)
 
-    season_df.to_sql(str(season_year) + "_season", sql_engine, **sql_options)  # truncates the table
+    season_name = "{}_season".format(season_year)
+    load_data_to_database(season_df, season_name, db_engine)
 
     grand_prixes = [gp.replace(' ', '-') for gp in season_df["Grand Prix"]]
 
     for gp in grand_prixes:
         soup = BeautifulSoup(urllib.request.urlopen(create_race_result_url(season_year, race_number, gp)),
                              features="lxml")
-
         gp_table = create_results_table(soup)
-        gp_df = pd.DataFrame(gp_table[1:], columns=gp_table[0])
+        gp_df = create_race_dataframe(gp_table)
         print(gp_df)
+        print(gp_df.dtypes)
 
-        gp_df.to_sql(str(season_year) + '_' + gp, sql_engine, **sql_options)
+        gp_name = "{}_{}".format(season_year, gp)
+        load_data_to_database(gp_df, gp_name, db_engine)
 
         race_number += 1
 
